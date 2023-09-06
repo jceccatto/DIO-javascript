b79e5250d4e60163732ac4b30f7c7db1c255769f
Jh.Ceccatto <jbom6@hotmail.com>
2023-09-06 10:58:38 -0300

    adicao aula 2

---
+++ DIO Sintaxe Básica em Javascript/aula 02/script.js
@@ -0,0 +1,103 @@
+//tipos primitivos
+
+//Boolean
+var vOuf = false;
+console.log(vOuf);
+console.log(typeof(vOuf));
+
+//number
+var numeroQualquer = 1;
+console.log(numeroQualquer);
+console.log(typeof(numeroQualquer));
+
+//string
+var nome = "JH";
+console.log(nome);
+console.log(typeof(nome));
+
+//Como declarar
+var variavel = 'JH';
+variavel = 'aloha';
+console.log(variavel);
+
+let variavel2 = 'JH';
+variavel2 = 'yup';
+console.log(variavel2);
+
+const constante = 'JH';
+console.log(constante);
+
+//escopo global e local
+
+var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+ escopoLocal();
+
+ //atribuição
+ var atribuicao = 'JH';
+
+ //comparação
+
+ var comparacao = '0' == 0;
+ console.log(comparacao);
+
+ var comparacaoIdentica = '0' === 0;
+ console.log(comparacaoIdentica);
+
+ //adicao
+ var adicao = 1 + 1;
+ console.log(adicao);
+
+ //subtracao
+ var subtracao = 2 - 1;
+ console.log(subtracao);
+
+ //multiplicacao 
+ var multiplicacao = 2 * 3;
+ console.log(multiplicacao);
+
+ //divisao Real
+ var divisaoReal = 6 + 2;
+ console.log(divisaoReal);
+
+ //divisao inteira
+ var divisaoInteira = 5 % 2;
+ console.log(divisaoInteira);
+
+ //potenciacao
+ var potenciacao = 2 ** 10;
+ console.log(potenciacao);
+
+ //Maior que
+ var maiorQue = 5 > 2;
+ console.log(maiorQue);
+
+ //Menor que
+ var menorQue = 5 < 2;
+ console.log(menorQue);
+
+//maior ou igual a
+ var maiorOuIgual = 5 >= 2;
+ console.log(maiorOuIgual);
+
+ //menor ou igual a
+ var menorOuIgual = 5 <= 2;
+ console.log(menorOuIgual);
+
+ //
+
+ var e = true && false;
+ console.log(e);
+
+ var ou = true & false;
+ console.log(ou);
+
+ var nao = !true;
+ console.log(nao);
+
+
